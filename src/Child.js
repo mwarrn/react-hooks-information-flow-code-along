29f28a2e600733218eae558e31801ed04acf828b
Michael Warren <mwarrn@amazon.com>
2022-06-18 15:29:40 -0700

    Completed assignment

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,35 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, color }) {
+  console.log(onChangeColor);
+
+  // invoke onChangeColor and pass in a random color as the argument
+  function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColor(newColor);
+  }
+
+  // * When the div in the Child component is clicked, it will use the
+  // onChangeColor variable to determine what function to run
+  // * onChangeColor is a prop that is passed down from the Parent
+  // component, which references the handleChangeColor function
+  // * The handleChangeColor function is the function that will actually
+  // run when the div is clicked, and will update state in the Parent component
+  // return (
+  //   <div
+  //     onClick={onChangeColor}
+  //     className="child"
+  //     style={{ backgroundColor: color }}
+  //   />
+  // );
+  return (
+    <div
+      onClick={handleClick}
+      className="child"
+      style={{ backgroundColor: color }}
+    />
+  );
 }
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -4,12 +4,26 @@ import Child from "./Child";
 
 function Parent() {
   const randomColor = getRandomColor();
+  // initial value for color state
   const [color, setColor] = useState(randomColor);
+  // store the color of the Child in the state of the Parent component
+  const [childrenColor, setChildrenColor] = useState("#FFF");
 
+
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    // update color state to a new value
+    setColor(newRandomColor);
+    setChildrenColor(newChildColor);
+  }
+
+  // We want to run handleChangeColor() function when either Child component
+  // is clicked. So we are going to pass this state changing function as a
+  // prop to both Child components.
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
     </div>
   );
 }
